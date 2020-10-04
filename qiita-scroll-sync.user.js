be46a1505ff35572c5bce22e375be9cc1c5366ae
MikuroXina <ryosukadnak@gmail.com>
2020-10-04 15:32:54 +0900

    :art: Refactor

--- qiita-scroll-sync.user.js
+++ qiita-scroll-sync.user.js
@@ -27,7 +27,86 @@ window.addEventListener("DOMContentLoaded", () => {
     const lineHeight = style.lineHeight ? parseFloat(style.lineHeight) : 21;
     const padding = style.padding ? parseFloat(style.padding) : 10;
 
-    // 見出し座標計算用のテキストエリアを作る
+    const textarea = createTextareaForCalculating(style, padding, lineHeight);
+    document.querySelector(".editorMarkdown_textareaWrapper").append(textarea);
+
+    const handleWheel = makeWheelHandler(editor, padding, lineHeight);
+
+    const handleScroll = makeScrollHandler(editor);
+
+    const handleInput = makeInputHandler(editor, padding, handleScroll);
+
+    const handleMutation = makeMutationHandler(editor, textarea, padding, handleScroll);
+
+    // エディタに各イベントリスナを設定する
+    editor.addEventListener("wheel", handleWheel);
+    editor.addEventListener("scroll", handleScroll);
+    editor.addEventListener("input", handleInput);
+
+    // プレビューの変更・レイアウトの変更・ウィンドウのリサイズを監視
+    new MutationObserver(handleMutation).observe(document.querySelector(".editorPreview"), {
+        childList: true,
+        subtree: true
+    });
+    window.addEventListener("resize", handleMutation);
+});
+
+function calcDeltaY(e, editor, lineHeight) {
+    if (e.deltaMode === WheelEvent.DOM_DELTA_PAGE) {
+        return editor.clientHeight;
+    }
+    if (e.deltaMode === WheelEvent.DOM_DELTA_LINE) {
+        return e.deltaY * lineHeight;
+    }
+    return e.deltaY;
+}
+
+// スクロールの下限をなくすように見せかける
+const makeWheelHandler = (editor, padding, lineHeight) => e => {
+    if (editor.scrollTop === editor.scrollHeight - editor.clientHeight) {
+        // スクロール末尾でなおスクロールしようとしている場合
+        const y = editor.style.paddingBottom ? parseFloat(editor.style.paddingBottom) : padding;
+        const deltaY = calcDeltaY(e, editor, lineHeight);
+        const sum = y + deltaY;
+        if (deltaY < 0 || sum < editor.clientHeight) {
+            // padding-bottomを増やしてスクロールしているように見せかける
+            editor.style.paddingBottom = sum + "px";
+            editor.scrollTop = editor.scrollHeight - editor.clientHeight;
+        }
+        return;
+    } 
+    if (parseFloat(editor.style.paddingBottom) !== padding) {
+        editor.style.paddingBottom = padding + "px";
+    }
+};
+
+
+// エディタにpadding-bottomを設定しているのをごまかす
+const makeInputHandler = (editor, padding, handleScroll) => () => {
+    if (editor.style.paddingBottom) {
+        const paddingBottom = parseFloat(editor.style.paddingBottom);
+        // padding-bottomが設定されている場合
+        if (paddingBottom > padding) {
+            // 入力時にpadding-bottomを調整して、パディングの中に文字列が隠れるのを防止する
+            const deltaY = editor.scrollTop - editor.scrollHeight + editor.clientHeight;
+            const y = paddingBottom + deltaY > padding ? paddingBottom + deltaY : padding;
+            editor.style.paddingBottom = y + "px";
+        }
+    }
+    const viewer = document.querySelector(".editorPreview_article");
+    if (viewer) {
+        // 入力のたびにプレビューのスクロール位置が0にされるのを無理やり修正
+        const disableScroll = () => {
+            // スクロール位置を再計算し、一度再計算したらイベントリスナを削除する
+            handleScroll();
+            viewer.removeEventListener("scroll", disableScroll);
+        };
+        viewer.addEventListener("scroll", disableScroll);
+    }
+};
+
+// 見出し座標計算用のテキストエリアを作る
+function createTextareaForCalculating(style, padding, lineHeight) {
     const textarea = document.createElement("textarea");
     Array.from(style).forEach(key => textarea.style.setProperty(key, style.getPropertyValue(key), style.getPropertyPriority(key)));
     textarea.style.pointerEvents = "none";
@@ -38,149 +117,112 @@ window.addEventListener("DOMContentLoaded", () => {
     textarea.style.width = "100%";
     textarea.style.height = (padding * 2 + lineHeight) + "px";
     textarea.readOnly = true;
-    document.querySelector(".editorMarkdown_textareaWrapper").append(textarea);
+    return textarea;
+}
 
-    // スクロールの下限をなくすように見せかける
-    const handleWheel = e => {
-        if (editor.scrollTop === editor.scrollHeight - editor.clientHeight) {
-            // スクロール末尾でなおスクロールしようとしている場合
-            const y = editor.style.paddingBottom ? parseFloat(editor.style.paddingBottom) : padding;
-            const deltaY = e.deltaMode === WheelEvent.DOM_DELTA_PAGE ? editor.clientHeight :
-                e.deltaMode === WheelEvent.DOM_DELTA_LINE ? e.deltaY * lineHeight :
-                    e.deltaY;
-            const sum = y + deltaY;
-            if (deltaY < 0 || sum < editor.clientHeight) {
-                // padding-bottomを増やしてスクロールしているように見せかける
-                editor.style.paddingBottom = sum + "px";
-                editor.scrollTop = editor.scrollHeight - editor.clientHeight;
+function calcScroll(x, y, editorScrollTop) {
+    const i = x.findIndex((value) => value <= editorScrollTop);
+    if (i === 0) {return 0}
+    if (i === x.length - 1 || i === -1) {return y[y.length - 1];}
+
+    return  (y[i + 1] - y[i]) / (x[i + 1] - x[i]) * (editorScrollTop - x[i]) + y[i];
+}
+
+// 見出しに合わせてスクロールする
+const makeScrollHandler = (editor) =>() => {
+    const viewer = document.querySelector(".editorPreview_article");
+    if (!viewer) {
+        // プレビュー非表示モードなら何もしない
+        return;
+    }
+
+    // 座標が未設定ならなにもしない
+    if (!editor.dataset.coordinates || !viewer.dataset.coordinates) {
+        return;
+    }
+
+    // datasetから各見出しの座標を取得
+    const x = JSON.parse(editor.dataset.coordinates);
+    const y = JSON.parse(viewer.dataset.coordinates);
+
+    // 線形補完でプレビューのスクロール位置を計算
+    viewer.scrollTop = calcScroll(x, y, editor.scrollTop);
+};
+
+// プレビューの変更時に見出しの座標を計算する
+const makeMutationHandler = (editor, textarea, padding, handleScroll) => async () => {
+    const viewer = document.querySelector(".editorPreview_article");
+    if (!viewer) {
+        // プレビュー非表示モードなら何もしない
+        return;
+    }
+    const target = viewer.children[0];
+
+    // エディターにおける各見出し位置を求める
+    const xCoordinates = getXCoordinates(editor, textarea, padding);
+
+    // プレビューにおける各見出し位置を求める
+    const yCoordinates = await getYCoordinates(target, viewer.scrollHeight);
+
+    // 座標をdata-coordinatesに設定
+    editor.dataset.coordinates = JSON.stringify(xCoordinates);
+    viewer.dataset.coordinates = JSON.stringify(yCoordinates);
+
+    // プレビューの下に空白を追加
+    target.style.marginBottom = viewer.clientHeight + "px";
+
+    // スクロール位置を修正
+    handleScroll();
+};
+
+// 画像の読み込みを待機
+function waitToLoadImages(target) {
+    const images = Array.from(target.querySelectorAll("img"));
+
+    return new Promise(resolve => {
+        const intervalID = setInterval(() => {
+            // naturalHeightが0より大きくなれば読み込み完了と推測
+            if (images.every(image => image.naturalHeight > 0)) {
+                // ループを終了
+                clearInterval(intervalID);
+                resolve();
             }
-        } else if (parseFloat(editor.style.paddingBottom) !== padding) {
-            editor.style.paddingBottom = padding + "px";
-        }
-    };
-
-    // エディタにpadding-bottomを設定しているのをごまかす
-    const handleInput = () => {
-        if (editor.style.paddingBottom) {
-            const paddingBottom = parseFloat(editor.style.paddingBottom);
-            // padding-bottomが設定されている場合
-            if (paddingBottom > padding) {
-                // 入力時にpadding-bottomを調整して、パディングの中に文字列が隠れるのを防止する
-                const deltaY = editor.scrollTop - editor.scrollHeight + editor.clientHeight;
-                const y = paddingBottom + deltaY > padding ? paddingBottom + deltaY : padding;
-                editor.style.paddingBottom = y + "px";
-            }
-        }
-        const viewer = document.querySelector(".editorPreview_article");
-        if (viewer) {
-            // 入力のたびにプレビューのスクロール位置が0にされるのを無理やり修正
-            const disableScroll = () => {
-                // スクロール位置を再計算し、一度再計算したらイベントリスナを削除する
-                handleScroll();
-                viewer.removeEventListener("scroll", disableScroll);
-            };
-            viewer.addEventListener("scroll", disableScroll);
-        }
-    };
-
-    // 見出しに合わせてスクロールする
-    const handleScroll = () => {
-        const viewer = document.querySelector(".editorPreview_article");
-        if (!viewer) {
-            // プレビュー非表示モードなら何もしない
-            return;
-        }
-
-        // 座標が未設定ならなにもしない
-        if (!editor.dataset.coordinates || !viewer.dataset.coordinates) {
-            return;
-        }
-
-        // datasetから各見出しの座標を取得
-        const x = JSON.parse(editor.dataset.coordinates);
-        const y = JSON.parse(viewer.dataset.coordinates);
-
-        // 線形補完でプレビューのスクロール位置を計算
-        const i = x.reduce((a, b, j) => b <= editor.scrollTop ? j : a, 0);
-        viewer.scrollTop = i === x.length - 1 ? y[y.length - 1] : (y[i + 1] - y[i]) / (x[i + 1] - x[i]) * (editor.scrollTop - x[i]) + y[i];
-    };
-
-    // プレビューの変更時に見出しの座標を計算する
-    const handleMutation = async () => {
-        const viewer = document.querySelector(".editorPreview_article");
-        if (!viewer) {
-            // プレビュー非表示モードなら何もしない
-            return;
-        }
-        const target = viewer.children[0];
-
-        // エディターにおける各見出し位置を求める
-        const getXCoordinates = new Promise(resolve => {
-            // 見出しで文章を分割
-            const re = /(?=^(?:> ?)?#+)/gm;
-            const paragraphs = editor.value.split(re);
-            const xCoordinates = paragraphs.map((paragraph, i) => {
-                // 計算用テキストエリアに入力、テキストエリアの高さから見出し位置を求める
-                textarea.value = paragraphs.slice(0, i + 1).join("");
-                return textarea.scrollHeight - padding * 2;
-            });
-
-            // スクロール先頭の座標を追加
-            if (paragraphs[0].match(re)) {
-                xCoordinates.unshift(0);
-            }
-            xCoordinates.unshift(0);
-
-            resolve(xCoordinates);
-        });
-
-        // プレビューにおける各見出し位置を求める
-        const getYCoordinates = new Promise(resolve => {
-            // detailsを全て開き、画像は全て先行読み込みに設定
-            target.querySelectorAll("details").forEach(node => (node.open = true));
-            target.querySelectorAll("img").forEach(node => (node.loading = "eager"));
-
-            // 画像の読み込みを待機
-            const images = Array.from(target.querySelectorAll("img"));
-            const intervalID = setInterval(() => {
-                // naturalHeightが0より大きくなれば読み込み完了と推測
-                if (images.every(image => image.naturalHeight > 0)) {
-                    // ループを終了
-                    clearInterval(intervalID);
-
-                    // 見出し位置を求める
-                    const headers = target.querySelectorAll("h1,h2,h3,h4,h5,h6");
-                    const yCoordinates = Array.from(headers).map(header => header.offsetTop);
-
-                    // スクロールの先頭と末尾の座標を追加
-                    yCoordinates.unshift(0);
-                    yCoordinates.push(viewer.scrollHeight);
-
-                    resolve(yCoordinates);
-                }
-            }, 10);
-        });
-
-        // 座標をdata-coordinatesに設定
-        editor.dataset.coordinates = JSON.stringify(await getXCoordinates);
-        viewer.dataset.coordinates = JSON.stringify(await getYCoordinates);
-
-        // プレビューの下に空白を追加
-        target.style.marginBottom = viewer.clientHeight + "px";
-
-        // スクロール位置を修正
-        handleScroll();
-    };
-
-    // エディタに各イベントリスナを設定する
-    editor.addEventListener("scroll", handleScroll);
-    editor.addEventListener("wheel", handleWheel);
-    editor.addEventListener("input", handleInput);
-
-    // プレビューの変更・レイアウトの変更・ウィンドウのリサイズを監視
-    new MutationObserver(handleMutation).observe(document.querySelector(".editorPreview"), {
-        childList: true,
-        subtree: true
+        }, 10);
     });
-    window.addEventListener("resize", handleMutation);
-});
+}
+
+function getXCoordinates(editor, textarea, padding) {
+    // 見出しで文章を分割
+    const re = /(?=^(?:> ?)?#+)/gm;
+    const paragraphs = editor.value.split(re);
+    const xCoordinates = paragraphs.map((_, i) => {
+        // 計算用テキストエリアに入力、テキストエリアの高さから見出し位置を求める
+        textarea.value = paragraphs.slice(0, i + 1).join("");
+        return textarea.scrollHeight - padding * 2;
+    });
+
+    // スクロール先頭の座標を追加
+    if (paragraphs[0].match(re)) {
+        xCoordinates.unshift(0);
+    }
+    xCoordinates.unshift(0);
+
+    return xCoordinates;
+}
+
+async function getYCoordinates(target, trailingY) {
+    target.querySelectorAll("details").forEach(node => (node.open = true));
+    target.querySelectorAll("img").forEach(node => (node.loading = "eager"));
+
+    await waitToLoadImages(target);
+
+    // 見出し位置を求める
+    const headers = target.querySelectorAll("h1,h2,h3,h4,h5,h6");
+    const yCoordinates = Array.from(headers).map(header => header.offsetTop);
+
+    // スクロールの先頭と末尾の座標を追加
+    yCoordinates.unshift(0);
+    yCoordinates.push(trailingY);
+
+    return yCoordinates;
+}
